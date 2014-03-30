c39fd754bfd0ca798e1f6c7e8a25add1ee2b4e10
snurakbill <snurkabill@gmail.com>
2014-03-30 15:32:24 +0200

    added tests for ReservationManager

---
+++ Hotel/src/test/java/com/mycompany/hotel/ReservationManagerTest.java
@@ -0,0 +1,93 @@
+package com.mycompany.hotel;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReservationManagerTest {
+	
+	private ReservationManager manager;
+	
+	@Before
+	public void setUp() {
+		manager = new ReservationManagerImpl();
+	}
+	
+	@Test
+	public void CreateReservationTest() {
+		
+		Reservation reservation = simpleReservationBuilder(1);		
+		manager.storeReservation(reservation);
+		
+		Reservation reservation2 = manager.findReservation(1);
+		
+		assertEquals(reservation.getResponsiblePerson(), reservation2.getResponsiblePerson());
+		assertEquals(reservation.getOtherPersons(), reservation2.getOtherPersons());
+		assertEquals(reservation.getAccount(), reservation2.getAccount());
+		assertEquals(reservation.getId(), reservation2.getId());
+		assertEquals(reservation.getDateOfCheckIn(), reservation2.getDateOfCheckIn());
+		assertEquals(reservation.getDuration(), reservation2.getDuration());
+		assertEquals(reservation.getNOBed(), reservation2.getNOBed());
+	}
+	
+	@Test(expected = IllegalArgumentException.class) 
+	public void deleteReservationWithWrongArgumentTest() {
+		manager.deleteReservation(null);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void deleteReservationTest() {
+		List<String> names = new ArrayList<>();
+		
+		Long id = new Long(1);
+		Integer roomId = new Integer(001);
+		Long acc = new Long(123456789);
+		Date checkIn = new Date(500000);
+		Date checkOut = new Date(600000);
+		
+		names.add("ahoj");
+		names.add("nazdar");
+		names.add("cau");
+		
+		Reservation reservation = simpleReservationBuilder(1);
+		manager.storeReservation(reservation);
+		
+		Reservation reservation2 = simpleReservationBuilder(2);
+		
+		manager.storeReservation(reservation2);
+		manager.deleteReservation(reservation);
+		
+		assertEquals(reservation2, manager.findReservation(2));
+		manager.findReservation(1);
+	}
+	@Test(expected = IllegalArgumentException.class) 
+	public void createReservationWithWrongArgumentTest() {
+		manager.storeReservation(null);
+	}
+	@Test(expected = IllegalArgumentException.class) 
+	public void updateReservatioinWithWrongArgumentTest() {
+		manager.editReservation(null);
+	}
+	@Test(expected = IllegalArgumentException.class) 
+	public void getReservationWithWrongArgumentTest() {
+		manager.findReservation(-1);
+	}
+	
+	private Reservation simpleReservationBuilder(int id) {
+		String acc = "123456789";
+		Date checkIn = new Date(500000);
+		String resPers = "ahoj";
+		List<String> names = new ArrayList<>();
+		names.add("lala");
+		names.add("nazdar");
+		names.add("cau");
+		
+		int duration = 10;
+		int nBeds = 4;
+		
+		return new Reservation(id, resPers, names, acc, checkIn, duration, nBeds);
+	}
+}
--- Hotel/src/test/java/com/mycompany/hotel/RoomManagerImplTest.java
+++ Hotel/src/test/java/com/mycompany/hotel/RoomManagerImplTest.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.hotel;
 
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import static junit.framework.Assert.assertEquals; // this is deprecated
 import org.junit.Before;
 import org.junit.Test;
